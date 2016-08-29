aaade9dde1a9134ceb207570762ecc7b77058d95
Ethan Xia <sanhust@gmail.com>
2016-08-29 08:43:18 +0800

    ALGO: Modify LIS.cpp to return the result sequences

--- algo/LIS.cpp
+++ algo/LIS.cpp
@@ -1,5 +1,5 @@
 // http://www.algorithmist.com/index.php/Longest_Increasing_Subsequence
-// LIS(i) = max{LIS(j)+1} (j<i and a[j]<a[i])
+// LIS(i) = max{LIS(j)+1} (0<=j<i and a[j]<a[i])
 
 #include <iostream>
 #include <vector>
@@ -9,39 +9,61 @@ using namespace std;
 
 class LIS {
 public:
-	int getLIS(vector<int> array);
+	int getLIS(vector<int> array, vector<int> &seq);
 };
 
-int LIS::getLIS(vector<int> array)
+int LIS::getLIS(vector<int> array, vector<int> &seq)
 {
 	int num = array.size();
 
 	// build a table with lengths of LIS for each index
 	// maxSeq[i]: the length of LIS ending with element numbered i
 	vector<int> maxSeq(num, 1);
+	vector<int> prevIndex(num, -1);
 
 	for (int i = 1; i < num; i++) {
-		int prevMax = maxSeq[i];
 		for (int j = i - 1; j >= 0; j--) {
-			if (array[i] > array[j]) {
-				prevMax = max(prevMax, maxSeq[j] + 1);
+			if (array[i] > array[j] && maxSeq[j] + 1 > maxSeq[i]) {
+				maxSeq[i] = maxSeq[j] + 1;
+				prevIndex[i] = j;
 			}
 		}
-
-		maxSeq[i] = prevMax;
 	}
 
-	return *max_element(maxSeq.begin(), maxSeq.end());
+	int result, index;
+	vector<int>::iterator iter = max_element(maxSeq.begin(), maxSeq.end());
+	result = *iter;
+	index = distance(maxSeq.begin(), iter);
+	while (index >= 0) {
+		seq.push_back(array[index]);
+		index = prevIndex[index];
+	}
+
+	return result;
 }
 
-int a[] = {4, 3, 1 ,2, 8, 3, 4};
-//int a[] =  {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
+//int a[] = {1, 4, 6, 2, 8, 9, 7};
+//int a[] = {4, 3, 1 ,2, 8, 3, 4};
+int a[] =  {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
 
 int main(int argc, char *argv[])
 {
 	int num = sizeof(a) / sizeof(a[0]);
 	LIS lis;
-	cout << lis.getLIS(vector<int>(a, a + num)) << endl;
+	vector<int> seq;
+
+	struct func {
+		void operator() (int i) {
+			cout << i << ' ';
+		}
+	} my_func;
+
+	cout << lis.getLIS(vector<int>(a, a + num), seq) << endl;
+
+	for_each(seq.rbegin(), seq.rend(), [](int i) {
+		cout << i << ' '; });
+//	for_each(seq.begin(), seq.end(), my_func);
+	cout << endl;
 
 	return 0;
 }
