f590beb4661a85802aee95dd6a75cb59419adfe9
Ethan Xia <sanhust@gmail.com>
2016-10-07 09:16:01 +0800

    CPP: Modify string.cpp and move_ctor.cpp

--- cpp/move_ctor.cpp
+++ cpp/move_ctor.cpp
@@ -1,19 +1,19 @@
 /*
- * =====================================================================================
+ * ============================================================================
  *
  *       Filename:  move_ctor.cpp
  *
- *    Description:  
+ *    Description:
  *
  *        Version:  1.0
  *        Created:  08/17/2015 08:16:50 PM
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
- *   Organization:  
+ *         Author:  YOUR NAME ()
+ *   Organization:
  *
- * =====================================================================================
+ * ============================================================================
  */
 
 // http://en.cppreference.com/w/cpp/language/move_constructor
@@ -60,7 +60,10 @@ int main()
 {
 	cout << "Trying to move A\n";
 	A a1 = f(A()); // move constructor from rvalue temporary
+	cout << "Before move, a1.s = " << a1.s << endl;
 	A a2 = move(a1);
+	cout << "After move, a1.s = " << a1.s << endl;
+	cout << "After move, a2.s = " << a2.s << endl;
 
 	cout << "Trying to move B\n";
 	B b1;
@@ -71,10 +74,14 @@ int main()
 	cout << "Trying to move C\n";
 	C c1;
 	C c2 = move(c1); // calls the copy ctor
+	cout << "After move, c1.s = " << c1.s << endl;
+	cout << "After move, c2.s = " << c2.s << endl;
 
 	cout << "Trying to move D\n";
 	D d1;
 	D d2 = move(d1);
+	cout << "After move, d1.s = " << d1.s << endl;
+	cout << "After move, d2.s = " << d2.s << endl;
 
 	return 0;
 }
--- cpp/string.cpp
+++ cpp/string.cpp
@@ -1,29 +1,105 @@
 /*
- * =====================================================================================
+ * ============================================================================
  *
  *       Filename:  string.cpp
  *
- *    Description:  
+ *    Description:
  *
  *        Version:  1.0
  *        Created:  09/06/2015 09:29:01 AM
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
- *   Organization:  
+ *         Author:  YOUR NAME ()
+ *   Organization:
  *
- * =====================================================================================
+ * ============================================================================
  */
 
 #include <iostream>
 #include <string>
 #include <cstdio>
+#include <chrono>
 
 using namespace std;
 
-int main(void)
+#ifdef _MSC_VER
+#define DONT_INLINE __declspec(noinline)
+#else
+#define DONT_INLINE __attribute__((__noinline__))
+#endif
+
+string s0;
+string s1;
+string s2;
+string s3;
+
+size_t totalLength = 0;
+
+template<char OP>
+void transfer(string &&from, string &to)
 {
+	if (OP == 'm') {
+		to = move(from);
+	} else if (OP == 'c') {
+		to = from;
+	} else if (OP == 'd') {
+		to.assign(from, 0, from.length());
+	} else {
+		from.swap(to);
+	}
+}
+
+template<char OP>
+DONT_INLINE
+void func3(string &&s)
+{
+	transfer<OP>(move(s), s3);
+	s3[0] = '3';
+	totalLength += s3.length();
+}
+
+template<char OP>
+DONT_INLINE
+void func2(string &&s)
+{
+	transfer<OP>(move(s), s2);
+	s2[0] = '2';
+	func3<OP>(move(s2));
+}
+
+template<char OP>
+DONT_INLINE
+void func1(string &&s)
+{
+	transfer<OP>(move(s), s1);
+	s1[0] = '1';
+	func2<OP>(move(s1));
+}
+
+template<char OP>
+void generate(size_t minSize, size_t iterations)
+{
+	for (size_t count = 0; count < iterations; ++count) {
+		s0.assign(minSize + count % 15, char('A' + count % 26));
+		func1<OP>(move(s0));
+	}
+}
+
+#if 0
+m 1 50000000
+c 1 50000000
+d 1 50000000
+s 1 50000000
+m 25 50000000
+c 25 50000000
+d 25 50000000
+s 25 50000000
+#endif
+
+int main(int argc, char *argv[])
+{
+#if 0
 	string str1 = "hello world"; // string str1("hello world");
 	string str2 = str1; // calling copy ctor
 
@@ -40,6 +116,46 @@ int main(void)
 	cout << "str2 addr: " << static_cast<void *>(&str2) << ' ' << str2.c_str() << endl;
 	printf("str1: %p\n", str1.c_str());
 	printf("str2: %p\n", str2.c_str());
+#endif
 
-	return 0;
+	if (argc != 4) {
+		cerr << "Usage: " << argv[0]
+			<< " <m|c|d|s> <min size> <iterations>" << endl
+			<< "Where: m = move" << endl
+			<< "       c = copy" << endl
+			<< "       d = copy-defeating-copy-on-write" << endl
+			<< "       s = swap" << endl;
+
+		return EXIT_FAILURE;
+	}
+
+	size_t minSize = static_cast<size_t>(atoi(argv[2]));
+	size_t iterations = static_cast<size_t>(atoi(argv[3]));
+
+	chrono::steady_clock::time_point startTime = chrono::steady_clock::now();
+
+	if (argv[1][0] = 'm') {
+		generate<'m'>(minSize, iterations);
+	} else if (argv[1][0] = 'c') {
+		generate<'c'>(minSize, iterations);
+	} else if (argv[1][0] = 'd') {
+		generate<'d'>(minSize, iterations);
+	} else {
+		generate<'s'>(minSize, iterations);
+	}
+
+	chrono::steady_clock::time_point endTime = chrono::steady_clock::now();
+
+	size_t durationMs = chrono::duration_cast<chrono::milliseconds>(endTime -
+			 startTime).count();
+
+	cout << "Time: " << durationMs << "ms" << endl;
+	cout << "Total length: " << totalLength << endl;
+
+	cout << s0 << endl;
+	cout << s1 << endl;
+	cout << s2 << endl;
+	cout << s3 << endl;
+
+	return EXIT_SUCCESS;
 }
